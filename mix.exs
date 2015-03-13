4234d60e0494362060c9e6545e67c57dfe2eb865
Martin Gutsch <gutsch@zwoelf.net>
2015-03-13 22:25:32 +0100

    require plug v.0.11.0

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule PlugSessionMemcached.Mixfile do
   defp deps do
     [
     {:cowboy, "~> 1.0.0"},
-    {:plug, "~> 0.9.0"},
+    {:plug, "~> 0.11.0"},
     {:mcd, github: "EchoTeam/mcd"}, # memcached driver
     ]
   end
