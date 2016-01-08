c897eb8d448a96f70d08313affdfff11d6eb5308
Martin Gutsch <gutsch@zwoelf.net>
2016-01-08 15:34:51 +0100

    finally have a test at hand

--- test/plug_session_memcached_test.exs
+++ test/plug_session_memcached_test.exs
@@ -1,7 +1,66 @@
-defmodule Plug.Session.MEMCACHED do
-  use ExUnit.Case
+defmodule Plug.Session.MEMCACHED.Test do
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  defmodule AppRouter do
+    use Plug.Router
+
+    plug :match
+    plug :dispatch
+
+    get "/hello" do
+      conn = Plug.Conn.fetch_session( conn )
+      conn = Plug.Conn.put_session( conn, "foo", "bar" )
+      send_resp(conn, 200, "world")
+    end
+
   end
+
+  @opts AppRouter.init([])
+  
+  def with_session(conn) do
+    session_opts = Plug.Session.init(
+      key: "_skeleton_key",
+      store: :memcached,
+      signing_salt: "SBRvpcSC",
+      table: :memcached_sessions,
+      encryption_salt: "Challala"
+    )
+    conn
+    |> Map.put(:secret_key_base, String.duplicate("abcdefgh", 8))
+    |> Plug.Session.call(session_opts)
+    |> Plug.Conn.fetch_session()
+    |> Plug.Conn.fetch_query_params()
+  end
+
+  def get_cookie_size conn do
+    conn.resp_headers 
+    |> Enum.filter( fn {"set-cookie", _ } -> true; _ -> false end )
+    |> Enum.at( 0 )
+    |> elem( 1 )
+    |> String.length
+  end
+
+  test "memcached-saved sessions shall have always the same cookie size" do
+    # Create a test connection
+    conn = with_session conn(:get, "/hello")
+    
+    # set "foo" key in get "/hello" (defined in router
+    conn = AppRouter.call(conn, @opts)
+    assert conn.private.plug_session["foo"] == "bar"
+    cookie_size_one_key = get_cookie_size conn
+
+    # set another key
+    conn = Plug.Conn.put_session( conn, "bang", String.duplicate("qwertzuiop", 20 ) )
+    assert conn.private.plug_session["bang"] == String.duplicate("qwertzuiop", 20 )
+    cookie_size_two_keys = get_cookie_size conn
+    assert cookie_size_one_key == cookie_size_two_keys
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "world"
+  end
+
 end
