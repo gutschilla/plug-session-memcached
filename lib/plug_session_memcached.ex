82e83cfb07491beef7f6a7b20faa6bcb2485ed6c
Martin Gutsch <gutsch@zwoelf.net>
2016-01-13 21:48:04 +0100

    add some basic inline docs

--- lib/plug_session_memcached.ex
+++ lib/plug_session_memcached.ex
@@ -1,6 +1,36 @@
 defmodule PlugSessionMemcached do
   use Application
 
+@vsn "0.3.3"
+    
+  @moduledoc """
+  This plug is to be used as store for Plug.Session. It saves session data in
+  a memcached instance providing just a session id being saved in the user's 
+  cookie. 
+  
+  ## Usage
+   1. Configure the connection to a memcached instance:
+      ```
+      config :plug_memcached_sessions, ['127.0.0.1', 11211 ]
+      ```
+    2. In your app, use and configure Plug.Session
+    ```
+    plug Plug.Session,
+      store: :memcached,
+      key: "_my_app_key", # use a proper value 
+      table: :memcached_sessions, # <-- this on is hard coded into the plug
+      signing_salt: "123456",   # use a proper value
+      encryption_salt: "654321" # use a proper value
+    ```
+  
+  ## Limitations
+  
+  Maximum session data size is 1MB. Be aware that this one MB is the size of 
+  the serialized data. Data serialization is done via Erlang's 
+  `term_to_binary(Data)` (see [mcd](https://github.com/EchoTeam/mcd/blob/master/src/mcd.erl#L715))
+  
+  """
+
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
